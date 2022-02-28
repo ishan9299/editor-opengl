392c5b1e3a00b3e8a4ec7c158519ce900c39926e
ishan <ishan365.ia@gmail.com>
2022-02-28 11:39:27 +0530

    add compiler switches to build.bat file

--- code/build.bat
+++ code/build.bat
@@ -2,6 +2,6 @@
 if not exist ..\build mkdir ..\build
 pushd ..\build
 
-cl /nologo /wd4100 /W4 /Z7 ..\code\win32_editor.cpp kernel32.lib opengl32.lib gdi32.lib user32.lib
+cl /nologo /wd4100 /W4 /Z7 /DEDITOR_OPENGL_DEBUG=1 /DEDITOR_USE_CGLM=0 ..\code\win32_editor.cpp kernel32.lib opengl32.lib gdi32.lib user32.lib
 
 popd
